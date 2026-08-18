45982e637fc0b296f7d38eecf5655d9745b97a60
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-08-18 10:41:03 -0300

    Add ChallengeNotFoundError, InvalidChallengeError, and InvalidSubmissionError domain errors

--- src/domain/entities/errors.ts
+++ src/domain/entities/errors.ts
@@ -21,3 +21,24 @@ export class InvalidBookError extends DomainError {
     this.name = "InvalidBookError";
   }
 }
+
+export class ChallengeNotFoundError extends DomainError {
+  constructor(slug: string) {
+    super(`Challenge with slug "${slug}" was not found`, "CHALLENGE_NOT_FOUND");
+    this.name = "ChallengeNotFoundError";
+  }
+}
+
+export class InvalidChallengeError extends DomainError {
+  constructor(message: string) {
+    super(message, "INVALID_CHALLENGE");
+    this.name = "InvalidChallengeError";
+  }
+}
+
+export class InvalidSubmissionError extends DomainError {
+  constructor(message: string) {
+    super(message, "INVALID_SUBMISSION");
+    this.name = "InvalidSubmissionError";
+  }
+}
